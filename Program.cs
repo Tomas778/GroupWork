b722c854b5092e7ecb0f63fb321315bc959ab23d
Tomas Stirblys <eternadeliux25@gmail.com>
2022-11-17 18:31:16 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupWork
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("*******************\n\n");
+            Skaiciavimas Musurez = new Skaiciavimas();
+            Musurez.Kintamasis1(); //call Inesa
+            Musurez.Kintamasis2(); //call Nerijus
+            Musurez.Kintamasis3(); //call Tomas
+
+            Musurez.Paskutinis();// ?????
+
+            Console.Write("*******************\n\n");
+        }
+    }
+}
---
+++ Skaiciavimas.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupWork
+{
+    public class Skaiciavimas
+    {
+        public void Kintamasis1()  //Inesa
+        {
+
+        }
+
+        public void Kintamasis2() //Nerijus
+        {
+
+        }
+
+        public void Kintamasis3() //Tomas
+        {
+
+        }
+
+        public void Paskutinis() //Tomas
+        {
+
+        }
+
+    }
+}
